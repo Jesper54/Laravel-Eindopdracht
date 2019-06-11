baaea1ab00805c53a078b5cde077e55601ac6770
Jesper54 <48125883+Jesper54@users.noreply.github.com>
2019-06-11 12:43:11 +0200

    commit

--- resources/views/createTopic.blade.php
+++ resources/views/createTopic.blade.php
@@ -14,22 +14,29 @@
                   <label for="username">Title</label>
                   <input type="title" class="form-control" id="username" placeholder="Title of the topic">
                 </div>
+
                 <div class="form-group">
                   <label for="category">Category</label>
-
                   <select class="form-control" id="exampleFormControlSelect1">
-                    <option value="0">Selecteer een onderwerp</option>
+                    <option value="0" disabled selected>Selecteer een onderwerp</option>
                     @foreach ($threads as $item)
                         <option value="{{ $item->id }}">{{ $item->title }}</option>
                     @endforeach
                   </select>
-
                 </div>
+
                 <div class="form-group">
-                  <label for="exampleFormControlTextarea1">Example textarea</label>
-                  <textarea class="form-control" id="exampleFormControlTextarea1" rows="10"></textarea>
+                    <label for="description">Description</label>
+                    <small style="color:red">(Max 120 characters)</small>
+                    <textarea class="form-control" id="description" rows="2" maxlength="120" placeholder="Description text"></textarea>
+                </div>
+
+                <div class="form-group">
+                  <label for="text">Text</label>
+                  <textarea class="form-control" id="text" rows="8" placeholder="The main text"></textarea>
                 </div>
                 <button type="submit" class="btn btn-primary">Create</button>
+                <button class="btn btn-warning">Cancel</button>
               </form>
 
 @endsection
